975f1ea91051eb9bfbcbba27792ddcf371230e01
Desmond Rivet <desmond.rivet@gmail.com>
2020-06-30 21:21:10 -0400

    initial dialog for action selection

--- src/ActionSelection.tsx
+++ src/ActionSelection.tsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import { ActionId, PlayerData, ViewableGameData } from "./models/game-data";
+import { ActionId, ViewableGameData } from "./models/game-data";
 import { ActionSelectionResponse, InputId } from "./models/inputs";
 import { customModalStyles } from "./view";
 import firebase from "./firebase";
 import Modal from 'react-modal';
 import { myUserId } from "./App";
+import { getAvailableActions } from "./models/action";
+import { FullPlayer } from "../functions/src/game";
 
 interface ActionSelectionProps {
     gameId: string;
     game: ViewableGameData;
-    player: PlayerData;
+    player: FullPlayer;
 }
 interface ActionSelectionState {
-    selectedAction: number;
+    selectedAction: ActionId;
     show: boolean;
     selected: boolean;
 }
@@ -28,7 +30,7 @@ function makeResponse(selectedAction: ActionId): ActionSelectionResponse {
 export class ActionSelection extends Component<ActionSelectionProps, ActionSelectionState> {
     state = {
         show: false,
-        selectedAction: 0,
+        selectedAction: ActionId.Nothing,
         selected: false
     };
 
@@ -51,27 +53,72 @@ export class ActionSelection extends Component<ActionSelectionProps, ActionSelec
     };
 
     private shouldShowActionSelection(): boolean {
+        if (!this.props.player) {
+            return false;
+        }
         const g = this.props.game;
         return g && g.inputRequest.userId === g.players[0].userId &&
             g.inputRequest.inputId === InputId.Action;
     }
 
     private renderModal() {
+        const availableActions = getAvailableActions(this.props.game, this.props.player);
         return (
             <Modal isOpen={this.state.show} style={customModalStyles}>
-                <h1 className={'text-xl mb-1'}>Please select your action</h1>
-                <div>Hello!</div>
-                <div className={'flex justify-center'}>
-                    <button className={'btn btn-std mr-1'}
-                            onClick={e => this.handleSelect(e)}>Select
-                    </button>
-                    <button className={'btn btn-std mr-1'}
-                            onClick={e => this.handleCancel()}>Cancel
-                    </button>
-                </div>
+                <form>
+                    <h1 className={'text-xl mb-1'}>Please select your action</h1>
+                    {this.renderActionList('Miscellaneous', availableActions.miscActions)}
+                    {this.renderActionList('Character', availableActions.characterActions)}
+                    {this.renderActionList('Title', availableActions.titleActions)}
+                    {this.renderActionList('Quorum', availableActions.quorumActions)}
+                    {this.renderActionList('Location', availableActions.locationActions)}
+                    {this.renderActionList('Skills', availableActions.skillActions)}
+                    {this.renderActionList('Loyalty', availableActions.loyaltyActions)}
+                    <div className={'flex justify-center'}>
+                        <button className={'btn btn-std mr-1'}
+                                onClick={e => this.handleSelect(e)}>Select
+                        </button>
+                        <button className={'btn btn-std mr-1'}
+                                onClick={e => this.handleCancel()}>Cancel
+                        </button>
+                    </div>
+                </form>
             </Modal>
         );
     }
+
+    private renderActionList(label: string, actions: ActionId[]) {
+        return(
+          <div className={'mb-4'}>
+              <div className={'mb-2 border-b-2 border-gray-600'}>{label}</div>
+              <div>
+                {actions.map(a => this.renderAction(a))}
+              </div>
+          </div>
+        );
+    }
+
+    private renderAction(action: ActionId) {
+        const actionValue: string = ActionId[action];
+        return (
+            <div>
+                <label>
+                    <input type="radio" name="actions" value={actionValue}
+                           checked={this.state.selectedAction === action}
+                           className="mr-1" onChange={this.handleOptionChange}/>
+                    {ActionId[action]}
+                </label>
+            </div>
+        );
+    }
+
+    handleOptionChange = changeEvent => {
+        const targetValue: string = changeEvent.target.value;
+        const action: ActionId = ActionId[targetValue];
+        this.setState({
+            selectedAction: action
+        });
+    };
 
     private handleCancel() {
         this.setState({
